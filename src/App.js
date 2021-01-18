3f1f26d64c1ce3a190f64725d44390a821557a4a
jamesncox <jamesncox@yahoo.com>
2021-01-18 10:14:36 -0600

    Create responsive.js and add min-width media query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './responsive.css';
 import Title from './components/layout/title.js'
 
 function App() {
--- src/components/layout/title.js
+++ src/components/layout/title.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Title() {
     return (
-        <h1 className="title">
-            The Boredinator
-        </h1>
+        <>
+            <h1 className="title">
+                The Boredinator
+            </h1>
+            <p>
+                Lets get off those screens and have some fun!
+            </p>
+        </>
     )
 }
