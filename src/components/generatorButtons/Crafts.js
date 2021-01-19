156457fe8aadb2a597f5452c8a58de8ce28ca0bd
jamesncox <jamesncox@yahoo.com>
2021-01-19 16:46:15 -0600

    Add SET_RANDOM_CRAFTS to Crafts.js

--- src/components/generatorButtons/Crafts.js
+++ src/components/generatorButtons/Crafts.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {
+    SET_RANDOM_CRAFTS
+} from '../../actionTypes'
 
 function Crafts(props) {
 
+    const handleClick = () => {
+        props.setRandomCrafts()
+    }
+
     return (
-        <button className="crafts-button">
+        <button
+            className="crafts-button"
+            onClick={e => handleClick(e)}
+        >
             Crafts
         </button>
     )
 }
 
-export default Crafts
+const mapDispatchToProps = dispatch = ({
+    setRandomCrafts: () => ({ type: SET_RANDOM_CRAFTS })
+})
+
+export default connect(null, mapDispatchToProps)(Crafts)
