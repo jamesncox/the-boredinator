562254711eecdf6f9d9b09c92b9744a5f00cab0d
jamesncox <jamesncox@yahoo.com>
2021-01-19 14:42:53 -0600

    Add SET_RANDOM_COUPLES to Couples.js

--- src/components/generatorButtons/Couples.js
+++ src/components/generatorButtons/Couples.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {
+    SET_RANDOM_COUPLES
+} from '../../actionTypes'
 
 function Couples(props) {
 
+    const handleClick = () => {
+        props.setRandomCouples()
+    }
+
     return (
-        <button className="couples-button">
+        <button
+            className="couples-button"
+            onClick={e => handleClick(e)}
+        >
             Couples
         </button>
     )
 }
 
-export default Couples
+const mapDispatchToProps = dispatch => ({
+    setRandomCouples: () => dispatch({ type: SET_RANDOM_COUPLES })
+})
+
+export default connect(Couples)
