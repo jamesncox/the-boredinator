46950784941e0f401f1e343c261d6a540b65d7f5
jamesncox <jamesncox@yahoo.com>
2021-01-19 10:58:34 -0600

    Create generatorButtons/Crafts.js and implement flex-grid

---
+++ src/components/generatorButtons/Crafts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Crafts(props) {
+
+    return (
+        <button className="crafts-button">
+            Crafts
+        </button>
+    )
+}
+
+export default Crafts
--- src/components/pageFormat/Home.js
+++ src/components/pageFormat/Home.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import RandomActivityButton from '../generatorButtons/RandomActivity'
 import CouplesButton from '../generatorButtons/Couples'
+import CraftsButton from '../generatorButtons/Crafts'
 import DisplayActivity from './DisplayActivity'
 
 export default function Home() {
 
     return (
         <>
-            <div className="category-grid">
-                <CouplesButton />
+            <div className="grid-wrapper">
+                <div className="category-grid">
+                    <CouplesButton />
+                    <CraftsButton />
+                </div>
             </div>
             <RandomActivityButton />
             <DisplayActivity />
