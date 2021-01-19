5f064fd2776d817f587cfae6e0087f535ceef06b
jamesncox <jamesncox@yahoo.com>
2021-01-19 17:13:14 -0600

    Add SET_RANDOM_FITNESS to Fitness.js

--- src/components/generatorButtons/Fitness.js
+++ src/components/generatorButtons/Fitness.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {
+    SET_RANDOM_FITNESS
+} from '../../actionTypes'
 
 function Fitness(props) {
 
+    const handleClick = () => {
+        props.setRandomFitness()
+    }
+
     return (
-        <button className="fitness-button">
+        <button
+            className="fitness-button"
+            onClick={e => handleClick(e)}
+        >
             Fitness
         </button>
     )
 }
 
-export default Fitness
+const mapDispatchToProps = dispatch => ({
+    setRandomFitness: () => dispatch({ type: SET_RANDOM_FITNESS })
+})
+
+export default connect(null, mapDispatchToProps)(Fitness)
