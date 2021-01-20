5076bf99d64d9c09e7cdc1432fc85747e2373cef
jamesncox <jamesncox@yahoo.com>
2021-01-20 09:06:58 -0600

    Add SET_RANDOM_OUTSIDE to Outside.js

--- src/components/generatorButtons/Outside.js
+++ src/components/generatorButtons/Outside.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {
+    SET_RANDOM_OUTSIDE
+} from '../../actionTypes'
 
 function Outside(props) {
 
+    const handleClick = () => {
+        props.setRandomOutside()
+    }
+
     return (
-        <button className="outside-button">
+        <button
+            className="outside-button"
+            onClick={e => handleClick(e)}
+        >
             Outside
         </button>
     )
 }
 
-export default Outside
+const mapDispatchToProps = dispatch => ({
+    setRandomOutside: () => dispatch({ type: SET_RANDOM_OUTSIDE })
+})
+
+export default connect(null, mapDispatchToProps)(Outside)
