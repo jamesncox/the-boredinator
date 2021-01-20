5fb19b29328beaf878726c7517be969f451d66cd
jamesncox <jamesncox@yahoo.com>
2021-01-20 09:25:52 -0600

    Add SET_RANDOM_PROJECTS to Projects.js

--- src/components/generatorButtons/Projects.js
+++ src/components/generatorButtons/Projects.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {
+    SET_RANDOM_PROJECTS
+} from '../../actionTypes'
 
 function Projects(props) {
 
+    const handleClick = () => {
+        props.setRandomProjects()
+    }
+
     return (
-        <button className="projects-button">
+        <button
+            className="projects-button"
+            onClick={e => handleClick(e)}
+        >
             Projects
         </button>
     )
 }
 
-export default Projects
+const mapDispatchToProps = dispatch => ({
+    setRandomProjects: () => dispatch({ type: SET_RANDOM_PROJECTS })
+})
+
+export default connect(null, mapDispatchToProps)(Projects)
