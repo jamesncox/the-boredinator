ef9ba56dc8bc3fb7a44c7e73ae49bcd3e0a0e0da
jamesncox <jamesncox@yahoo.com>
2021-01-20 09:35:27 -0600

    Add SET_RANDOM_SOLO to Solo.js

--- src/components/generatorButtons/Solo.js
+++ src/components/generatorButtons/Solo.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {
+    SET_RANDOM_SOLO
+} from '../../actionTypes'
 
 function Solo(props) {
 
+    const handleClick = () => {
+        props.setRandomSolo()
+    }
+
     return (
-        <button className="solo-button">
+        <button
+            className="solo-button"
+            onClick={e => handleClick(e)}
+        >
             Solo
         </button>
     )
 }
 
-export default Solo
+const mapDispatchToProps = dispatch => ({
+    setRandomSolo: () => dispatch({ type: SET_RANDOM_SOLO })
+})
+
+export default connect(null, mapDispatchToProps)(Solo)
