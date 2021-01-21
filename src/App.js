8d5f2504bc0498e0844a4966ade8955ce073b44b
jamesncox <jamesncox@yahoo.com>
2021-01-21 12:58:04 -0600

    Add react-router-dom and import to App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import './stylesheets/categories.css'
 import Title from './components/pageFormat/Title.js'
 import Home from './components/pageFormat/Home'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
