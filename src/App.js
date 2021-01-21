e9b3db577516cb530654a26c4c6869e4c7bd7698
jamesncox <jamesncox@yahoo.com>
2021-01-21 10:23:43 -0600

    Comment out light-dark-theme from App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './stylesheets/App.css';
 import './stylesheets/responsive.css';
 import './stylesheets/categories.css'
-import './stylesheets/light-dark-theme.css'
+// import './stylesheets/light-dark-theme.css'
 import Title from './components/pageFormat/Title.js'
 import Home from './components/pageFormat/Home'
+import ThemeSwitcher from './components/pageFormat/ThemeSwitcher'
 
 function App() {
   return (
     <div className="App">
+      <ThemeSwitcher />
       <Title />
       <Home />
     </div>
---
+++ src/components/pageFormat/ThemeSwitcher.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ThemeSwitcher(props) {
+
+    return (
+        <div></div>
+    )
+
+}
+
+export default ThemeSwitcher
