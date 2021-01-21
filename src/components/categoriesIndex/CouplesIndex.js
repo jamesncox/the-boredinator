48542eb00c703839794f4896f482b51113529fa8
jamesncox <jamesncox@yahoo.com>
2021-01-21 12:52:02 -0600

    Import activitiesData to CouplesIndex, map and display <li>

--- src/components/categoriesIndex/CouplesIndex.js
+++ src/components/categoriesIndex/CouplesIndex.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import activitiesData from '../../data/activities.json'
 
 function CouplesIndex(props) {
+
+    const couplesActivities = activitiesData.filter(a => {
+        return a.categories.includes("couples")
+    })
+
+    return (
+        <div>
+            <p>Couples</p>
+            <ul>
+                {couplesActivities.map(activity => {
+                    return <li>{activity.name}</li>
+                })}
+            </ul>
+        </div>
+    )
 
 }
 
--- src/components/pageFormat/IndexPage.js
+++ src/components/pageFormat/IndexPage.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import CouplesIndex from '../categoriesIndex/CouplesIndex'
 
 function IndexPage(props) {
 
+    return (
+        <>
+            <CouplesIndex />
+        </>
+    )
 }
 
 export default IndexPage
