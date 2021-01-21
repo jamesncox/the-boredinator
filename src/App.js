a0892d20679739a38f3017db5d817562f861ce73
jamesncox <jamesncox@yahoo.com>
2021-01-21 09:52:14 -0600

    Create light-dark-theme.css and import to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './stylesheets/App.css';
 import './stylesheets/responsive.css';
 import './stylesheets/categories.css'
+import './stylesheets/light-dark-theme.css'
 import Title from './components/pageFormat/Title.js'
 import Home from './components/pageFormat/Home'
 
