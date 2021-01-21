8b3cf1fa78a2dcca2ae4e5e897dcebf8aa9d45fc
jamesncox <jamesncox@yahoo.com>
2021-01-21 13:04:08 -0600

    Import IndexPage to App.js, add Switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import './stylesheets/App.css';
-import './stylesheets/responsive.css';
-import './stylesheets/categories.css'
-import Title from './components/pageFormat/Title.js'
-import Home from './components/pageFormat/Home'
-
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
+import './stylesheets/App.css';
+import './stylesheets/responsive.css';
+import './stylesheets/categories.css'
+import Title from './components/pageFormat/Title.js'
+import Home from './components/pageFormat/Home'
+import IndexPage from './components/pageFormat/IndexPage'
 
 function App() {
   return (
-    <div className="App">
-      <Title />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Title />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/index">
+            <IndexPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
