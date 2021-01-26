820879a683f7374d9642cbb21dff7927a5c4b257
jamesncox <jamesncox@yahoo.com>
2021-01-26 15:04:11 -0600

    Update Toggle.js debugging input checkbox not persisting

--- src/components/pageFormat/Toggle.js
+++ src/components/pageFormat/Toggle.js
@@ -1,34 +1,34 @@
 import React from 'react'
 
-const Toggle = () => {
+export default function Toggle() {
 
-    let clickedClass = "clicked";
-    const body = document.body;
-    const lightTheme = "light";
-    const darkTheme = "dark";
-    let theme;
+    let clickedClass = "clicked"
+    const body = document.body
+    const lightTheme = "light"
+    const darkTheme = "dark"
+    let theme
 
     if (localStorage) {
-        theme = localStorage.getItem("theme");
+        theme = localStorage.getItem("theme")
     }
 
     if (theme === lightTheme || theme === darkTheme) {
-        body.classList.add(theme);
+        body.classList.add(theme)
     } else {
-        body.classList.add(lightTheme);
+        body.classList.add(lightTheme)
     }
 
     const switchTheme = (e) => {
         if (theme === darkTheme) {
-            body.classList.replace(darkTheme, lightTheme);
-            e.target.classList.remove(clickedClass);
-            localStorage.setItem("theme", "light");
-            theme = lightTheme;
+            body.classList.replace(darkTheme, lightTheme)
+            e.target.classList.remove(clickedClass)
+            localStorage.setItem("theme", "light")
+            theme = lightTheme
         } else {
-            body.classList.replace(lightTheme, darkTheme);
-            e.target.classList.add(clickedClass);
-            localStorage.setItem("theme", "dark");
-            theme = darkTheme;
+            body.classList.replace(lightTheme, darkTheme)
+            e.target.classList.add(clickedClass)
+            localStorage.setItem("theme", "dark")
+            theme = darkTheme
         }
     }
 
@@ -37,6 +37,7 @@ const Toggle = () => {
             <input
                 type="checkbox"
                 id="toggle"
+                aria-label="toggle dark mode"
                 className={theme === "dark" ? clickedClass : ""}
                 onClick={(e) => switchTheme(e)}
             />
@@ -44,5 +45,3 @@ const Toggle = () => {
         </div>
     )
 }
-
-export default Toggle
