e0e26a55243cb4aa0e9770da0521e8b8c22b420f
jamesncox <jamesncox@yahoo.com>
2021-01-26 17:43:26 -0600

    Debug getElementById(toggle-switch) to persist toggle

--- src/components/pageFormat/Toggle.js
+++ src/components/pageFormat/Toggle.js
@@ -10,6 +10,7 @@ export default function Toggle() {
 
     if (localStorage) {
         theme = localStorage.getItem("theme")
+        // console.log(document.getElementsByClassName("toggle")[0])
     }
 
     if (theme === lightTheme || theme === darkTheme) {
@@ -21,27 +22,28 @@ export default function Toggle() {
     const switchTheme = (e) => {
         if (theme === darkTheme) {
             body.classList.replace(darkTheme, lightTheme)
-            e.target.classList.remove(clickedClass)
+            // e.target.classList.remove(clickedClass)
             localStorage.setItem("theme", "light")
             theme = lightTheme
         } else {
             body.classList.replace(lightTheme, darkTheme)
-            e.target.classList.add(clickedClass)
+            // e.target.classList.add(clickedClass)
             localStorage.setItem("theme", "dark")
             theme = darkTheme
         }
+        // document.getElementById("toggle").checked = true;
     }
 
     return (
         <div className="toggle">
             <input
                 type="checkbox"
-                id="toggle"
+                id="toggle-switch"
                 aria-label="toggle dark mode"
-                className={theme === "dark" ? clickedClass : ""}
+                // className={theme === "dark" ? clickedClass : ""}
                 onClick={(e) => switchTheme(e)}
             />
-            <label htmlFor="toggle"></label>
+            <label htmlFor="toggle-switch"></label>
         </div>
     )
 }
