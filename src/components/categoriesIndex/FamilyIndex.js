20646f169d588cdb2376e1aa6309a86af2f7a815
jamesncox <jamesncox@yahoo.com>
2021-02-02 16:20:15 -0600

    Update FamilyIndex with Link and remove connect

--- src/components/categoriesIndex/FamilyIndex.js
+++ src/components/categoriesIndex/FamilyIndex.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import {
-    SET_SELECTED_INDEX_ACTIVITY
-} from '../../actionTypes'
+import { Link } from 'react-router-dom'
 import activitiesData from '../../data/activities.json'
 
 function FamilyIndex(props) {
@@ -17,10 +14,6 @@ function FamilyIndex(props) {
         return 0;
     })
 
-    const handleClick = (id) => {
-        props.setSelectedIndexActivity(id)
-    }
-
     return (
         <>
             <p className="index-category-title">Family</p>
@@ -28,13 +21,14 @@ function FamilyIndex(props) {
                 <ol style={{ listStyleType: "none" }}>
                     {alphabetizedActivities.map(activity => {
                         return (
-                            <p
-                                key={activity.id}
-                                className="index-list-items"
-                                onClick={e => handleClick(activity.id)}
-                            >
-                                {activity.name}
-                            </p>
+                            <Link key={activity.id} to={`/activities/${activity.id}`}>
+                                <p
+                                    key={activity.id}
+                                    className="index-list-items"
+                                >
+                                    {activity.name}
+                                </p>
+                            </Link>
                         )
                     })}
                 </ol>
@@ -44,8 +38,4 @@ function FamilyIndex(props) {
 
 }
 
-const mapDispatchToProps = dispatch => ({
-    setSelectedIndexActivity: (id) => dispatch({ type: SET_SELECTED_INDEX_ACTIVITY, payload: id })
-})
-
-export default connect(null, mapDispatchToProps)(FamilyIndex)
+export default FamilyIndex
