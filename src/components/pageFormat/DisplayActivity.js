2103bb45d706a01003dd7808847c6b04c6e17dc0
jamesncox <jamesncox@yahoo.com>
2021-04-04 13:25:54 -0500

    Format DisplayActivity with Prettier

--- src/components/pageFormat/DisplayActivity.js
+++ src/components/pageFormat/DisplayActivity.js
@@ -1,60 +1,54 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import {
-    CLEAR_SELECTED_ACTIVITY
-} from '../../actionTypes'
-import ImageLoad from '../hooks/ImageLoad'
-import BlurryImage from '../../assets/blurry image small.jpg'
+import React from "react";
+import { connect } from "react-redux";
+import { CLEAR_SELECTED_ACTIVITY } from "../../actionTypes";
+import ImageLoad from "../hooks/ImageLoad";
+import BlurryImage from "../../assets/blurry image small.jpg";
 
 function DisplayActivity(props) {
+  const handleClick = () => {
+    props.clearSelectedActivity();
+  };
 
-    const handleClick = () => {
-        props.clearSelectedActivity()
-    }
-
-    if (props.activity.length === 0) {
-        return null
-    } else {
-        return (
-            <>
-                <p className="activity-title">{props.activity[0].name.toUpperCase()}</p>
-                <div className="card-wrapper">
-                    <div className="card">
-                        <ImageLoad
-                            src={props.activity[0].image}
-                            placeholder={BlurryImage}
-                            alt={props.activity[0].alt}
-                            className="activity-image"
-                        />
-                        <div className="container">
-                            <p className="description">{props.activity[0].description}</p>
-                            <form
-                                action={props.activity[0].url}
-                                target="_blank"
-                                rel="noreferrer noopener"
-                            >
-                                <button className="take-me-there">Take Me There!</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <button
-                    className="go-back-btn"
-                    onClick={e => handleClick(e)}
-                >
-                    Go Back
-                </button>
-            </>
-        )
-    }
+  if (props.activity.length === 0) {
+    return null;
+  } else {
+    return (
+      <>
+        <p className="activity-title">{props.activity[0].name.toUpperCase()}</p>
+        <div className="card-wrapper">
+          <div className="card">
+            <ImageLoad
+              src={props.activity[0].image}
+              placeholder={BlurryImage}
+              alt={props.activity[0].alt}
+              className="activity-image"
+            />
+            <div className="container">
+              <p className="description">{props.activity[0].description}</p>
+              <form
+                action={props.activity[0].url}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <button className="take-me-there">Take Me There!</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <button className="go-back-btn" onClick={(e) => handleClick(e)}>
+          Go Back
+        </button>
+      </>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-    activity: state.activities.activity
-})
+const mapStateToProps = (state) => ({
+  activity: state.activities.activity,
+});
 
-const mapDispatchToProps = dispatch => ({
-    clearSelectedActivity: () => dispatch({ type: CLEAR_SELECTED_ACTIVITY })
-})
+const mapDispatchToProps = (dispatch) => ({
+  clearSelectedActivity: () => dispatch({ type: CLEAR_SELECTED_ACTIVITY }),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(DisplayActivity)
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayActivity);
