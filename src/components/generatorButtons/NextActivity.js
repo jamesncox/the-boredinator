73b52341bb58714c2a5c32cf63771438708c52e5
jamesncox <jamesncox@yahoo.com>
2021-04-04 14:01:15 -0500

    Create NextActivity.js

---
+++ src/components/generatorButtons/NextActivity.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { NEXT_ACTIVITY } from "../../actionTypes";
+
+function NextActivity(props) {
+  const handleClick = () => {
+    props.nextActivity();
+  };
+
+  return (
+    <button className="random-activity-btn" onClick={(e) => handleClick(e)}>
+      Next Activity
+    </button>
+  );
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  nextActivity: () => dispatch({ type: NEXT_ACTIVITY }),
+});
+
+export default connect(null, mapDispatchToProps)(NextActivity);
