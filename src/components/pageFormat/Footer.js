a3478719b2d748640b6b40897c149c19d672c17e
jamesncox <jamesncox@yahoo.com>
2021-04-04 21:04:49 -0500

    Update Footer.js --link-hover

--- src/components/pageFormat/Footer.js
+++ src/components/pageFormat/Footer.js
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Made with ❤️‍ by James Cox</p>
+      <p>
+        Made with ❤️‍ by{" "}
+        <a
+          className="footer-link"
+          href="https://www.jamescox.dev/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          James Cox
+        </a>
+      </p>
       <p>
         {"Copyright © "}
         {new Date().getFullYear()}
