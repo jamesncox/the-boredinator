dce75a417c9728466ac6dac5dc5b2e93a06be5ac
jamesncox <jamesncox@yahoo.com>
2021-04-04 20:42:23 -0500

    Update App.css with .footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-import './stylesheets/App.css';
-import './stylesheets/responsive.css';
-import './stylesheets/categories.css'
-import './stylesheets/toggle.css'
-import Title from './components/pageFormat/Title.js'
-import Home from './components/pageFormat/Home'
-import IndexPage from './components/pageFormat/IndexPage'
-import { ActivityPage } from './components/pageFormat/ActivityPage'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./stylesheets/App.css";
+import "./stylesheets/responsive.css";
+import "./stylesheets/categories.css";
+import "./stylesheets/toggle.css";
+import Title from "./components/pageFormat/Title.js";
+import Home from "./components/pageFormat/Home";
+import IndexPage from "./components/pageFormat/IndexPage";
+import { ActivityPage } from "./components/pageFormat/ActivityPage";
+import Footer from "./components/pageFormat/Footer";
 
 function App() {
   return (
@@ -25,8 +22,13 @@ function App() {
           <Route exact path="/activities">
             <IndexPage />
           </Route>
-          <Route exact path="/activities/:activityId" component={ActivityPage} />
+          <Route
+            exact
+            path="/activities/:activityId"
+            component={ActivityPage}
+          />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
